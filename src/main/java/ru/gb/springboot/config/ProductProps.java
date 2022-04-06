7ea77cbb112aa48f2121b51dfbfa52db4c705c54
Ond Re <aapetrulevich@icloud.com>
2022-04-06 14:10:53 +0300

    HomeTask4

--- src/main/java/ru/gb/springboot/config/ProductProps.java
+++ src/main/java/ru/gb/springboot/config/ProductProps.java
@@ -1,5 +1,15 @@
 package ru.gb.springboot.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import ru.gb.springboot.model.Product;
 
+import java.util.List;
+
+@ConfigurationProperties("shop.properties")
+@Getter
+@Setter
 public class ProductProps {
+    private List<Product> products;
 }
--- src/main/java/ru/gb/springboot/controller/MessageController.java
+++ src/main/java/ru/gb/springboot/controller/MessageController.java
@@ -1,15 +1,27 @@
 package ru.gb.springboot.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.gb.springboot.config.ProductProps;
+
+import java.util.Collections;
 
 @Controller
+@RequiredArgsConstructor
 public class MessageController {
+
+    private final ProductProps productProps;
 
     @GetMapping("/message")
     public String helloMessageJsp(Model model) {
-        model.addAttribute("msg", "hello spring boot");
+        if (!CollectionUtils.isEmpty(productProps.getProducts())) {
+            model.addAttribute("msg", productProps.getProducts().size());
+        }
         return "message";
     }
+
+
 }
